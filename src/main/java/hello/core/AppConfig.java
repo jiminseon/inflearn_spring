71cdcb11431192878a153b6cb067ca57ff86960f
minsun <pitapatsun@gmail.com>
2024-08-20 15:46:46 +0900

    refactor: Appconfig 설계 잘보이도록 리팩토링

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,6 +1,8 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberSerive;
 import hello.core.member.MemberServiceImple;
 import hello.core.member.MemoryMemberRepository;
@@ -10,11 +12,18 @@ import hello.core.order.OrderServiceImpl;
 public class AppConfig {
 
     public MemberSerive memberSerive() {
-        return new MemberServiceImple(new MemoryMemberRepository());
+        return new MemberServiceImple(memberRepository());
+    }
+
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    public DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
+    }
 }
