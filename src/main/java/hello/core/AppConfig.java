3b560ff74c91be3f038f92c5ca0fe9a8aa5a851e
minsun <pitapatsun@gmail.com>
2024-08-22 16:56:36 +0900

    test: @Configuration과 Singleton

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -14,17 +14,24 @@ import org.springframework.context.annotation.Configuration;
 @Configuration //설정정보
 public class AppConfig {
 
+    //call AppConfig.memberService
+    // call AppConfig.memberRepository
+    // call AppConfig.orderService
+
     @Bean //메소드 이름이 spring 컨테이너에 저장됨
     public MemberService memberService() {
+        System.out.println("call AppConfig.memberService");
         return new MemberServiceImple(memberRepository());
     }
     @Bean
     public MemberRepository memberRepository() {
+        System.out.println("call AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
 
     @Bean
     public OrderService orderService() {
+        System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
