09eac33f58dc47b5c4d6e7de43b3f3012ad391fa
minsun <pitapatsun@gmail.com>
2024-08-25 15:25:50 +0900

    refactor: 초기화, 소멸 메서드

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,9 +1,7 @@
 package hello.core.lifecycle;
 
-import org.springframework.beans.factory.DisposableBean;
-import org.springframework.beans.factory.InitializingBean;
 
-public class NetworkClient implements InitializingBean, DisposableBean {
+public class NetworkClient {
 
     private String url;
 
@@ -31,16 +29,15 @@ public class NetworkClient implements InitializingBean, DisposableBean {
         System.out.println("close: " + url);
     }
 
-    @Override
-    public void afterPropertiesSet() throws Exception {
+    public void init() {
         //의존관계 주입 다 끝나고 초기화 시작
-        System.out.println("NetworkClient.afterPropertiesSet");
+        System.out.println("NetworkClient.init");
         connect();
         call("초기화 연결 메시지");
     }
-    @Override
-    public void destroy() throws Exception {
-        System.out.println("NetworkClient.destory");
+
+    public void close() {
+        System.out.println("NetworkClient.close");
         disconnect();
     }
 }
